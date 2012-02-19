4722d5bbba6ebe6c6251d38331f89a8bf0bbdc17
Simon Goller <neosam@gmail.com>
2012-02-19 16:33:51 +0100

    Added animator.

---
+++ src/com/gollersoft/sprite/UGSpriteAnimation.java
@@ -0,0 +1,60 @@
+package com.gollersoft.sprite;
+
+import com.gollersoft.jultragame.core.UGFinalRect;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neosam
+ * Date: 2/19/12
+ * Time: 4:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UGSpriteAnimation {
+    final UGFinalRect begin;
+    final int offset;
+    final int length;
+
+    private int x;
+    private int next;
+
+    private int counter;
+
+    public UGSpriteAnimation(UGFinalRect begin, int offset, int length) {
+        this.begin = begin;
+        this.offset = offset;
+        this.length = length;
+        x = begin.x;
+        next = 1;
+        counter = 0;
+    }
+
+    public void step() {
+        counter++;
+        if (counter == length) {
+            counter = 0;
+            x += offset + begin.width;
+        }
+    }
+
+   public UGFinalRect getPosition() {
+       return new UGFinalRect(x, begin.y, begin.width, begin.height);
+   }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        if (x > 0 && x < length)
+        this.x = x;
+    }
+
+    public int getNext() {
+        return next;
+    }
+
+    public void setNext(int next) {
+        if (next > 0)
+            this.next = next;
+    }
+}
