800618e228d54451dc825d4a8cc9a939f38f6d42
Simon Goller <neosam@gmail.com>
2012-02-20 23:05:17 +0100

    Added the LayerList.
    To store the different layers we need to store them in a list which is able to call the draw methods its layers.

---
+++ src/com/gollersoft/jultragame/layer/UGLayerList.java
@@ -0,0 +1,40 @@
+package com.gollersoft.jultragame.layer;
+
+import com.gollersoft.jultragame.core.UG;
+import com.gollersoft.jultragame.core.UGList;
+import com.gollersoft.jultragame.core.display.UGGraphics;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neosam
+ * Date: 2/20/12
+ * Time: 10:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UGLayerList implements UGList<UGLayer> {
+    private final UGList<UGLayer> layers;
+
+    public UGLayerList(UG ug) {
+        layers = ug.createList();
+    }
+
+    @Override
+    public int size() {
+        return layers.size();
+    }
+
+    @Override
+    public void add(UGLayer ugLayer) {
+        layers.add(ugLayer);
+    }
+
+    @Override
+    public UGLayer at(int i) {
+        return layers.at(i);
+    }
+
+    public void draw(UGGraphics g) {
+        for (int i = 0; i < layers.size(); i++)
+            layers.at(i).draw(g);
+    }
+}
