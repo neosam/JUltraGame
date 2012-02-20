ff2639c144486d8c10d01a8a2c016055169c090d
Simon Goller <neosam@gmail.com>
2012-02-20 22:54:52 +0100

    First Layer.
    Introduced UGLayer, a abstract class to display a layer.

---
+++ src/com/gollersoft/jultragame/layer/UGLayer.java
@@ -0,0 +1,14 @@
+package com.gollersoft.jultragame.layer;
+
+import com.gollersoft.jultragame.core.display.UGGraphics;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neosam
+ * Date: 2/20/12
+ * Time: 10:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+abstract public class UGLayer {
+    abstract void draw(UGGraphics g);
+}
