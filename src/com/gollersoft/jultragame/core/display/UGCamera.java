3c46ff3280587ccbe1711b4ad82b57deb6294133
Simon Goller <neosam@gmail.com>
2012-02-21 19:34:11 +0100

    Introduced a camera
    Camera will be needed for positioning later in the game.

---
+++ src/com/gollersoft/jultragame/core/display/UGCamera.java
@@ -0,0 +1,14 @@
+package com.gollersoft.jultragame.core.display;
+
+import com.gollersoft.jultragame.core.UGPoint;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neosam
+ * Date: 2/21/12
+ * Time: 7:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UGCamera extends UGPoint {
+
+}
---
+++ src/com/gollersoft/jultragame/layer/UGImageScrollLayer.java
@@ -0,0 +1,22 @@
+package com.gollersoft.jultragame.layer;
+
+import com.gollersoft.jultragame.core.display.UGGraphics;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neosam
+ * Date: 2/21/12
+ * Time: 7:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UGImageScrollLayer implements UGLayer {
+    @Override
+    public void draw(UGGraphics g) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void frame() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
