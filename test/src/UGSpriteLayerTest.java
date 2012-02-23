d896e087a4c285c33b990ba199a9c54aaef29770
Simon Goller <neosam@gmail.com>
2012-02-23 20:20:24 +0100

    Added unit test for UGSpriteLayer

---
+++ test/src/UGSpriteLayerTest.java
@@ -0,0 +1,64 @@
+import com.gollersoft.jultragame.binding.swing.UGSwing;
+import com.gollersoft.jultragame.core.display.UGGraphics;
+import com.gollersoft.jultragame.layer.UGSpriteLayer;
+import com.gollersoft.jultragame.sprite.UGSprite;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.easymock.EasyMock.*;
+import static sun.plugin2.liveconnect.ArgumentHelper.validate;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neosam
+ * Date: 2/23/12
+ * Time: 8:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UGSpriteLayerTest {
+    private UGSpriteLayer spriteLayer;
+
+    @Before
+    public void setUp() throws Exception {
+        spriteLayer = new UGSpriteLayer(new UGSwing(1, 1));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        spriteLayer = null;
+
+    }
+
+    @Test
+    public void testDraw() throws Exception {
+        UGSprite aSprite = createMock(UGSprite.class);
+        UGSprite bSprite = createMock(UGSprite.class);
+        UGGraphics graphics = createMock(UGGraphics.class);
+        spriteLayer.add(aSprite);
+        spriteLayer.add(bSprite);
+        aSprite.draw(graphics);
+        bSprite.draw(graphics);
+        replay(aSprite);
+        replay(bSprite);
+        spriteLayer.draw(graphics);
+        verify(aSprite);
+        verify(bSprite);
+
+    }
+
+    @Test
+    public void testFrame() throws Exception {
+        UGSprite aSprite = createMock(UGSprite.class);
+        UGSprite bSprite = createMock(UGSprite.class);
+        spriteLayer.add(aSprite);
+        spriteLayer.add(bSprite);
+        aSprite.step();
+        bSprite.step();
+        replay(aSprite);
+        replay(bSprite);
+        spriteLayer.frame();
+        verify(aSprite);
+        verify(bSprite);
+    }
+}
