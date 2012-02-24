0aef1c5cf68c98d4f3ee59239cc8f5ec12a8cbb0
Simon Goller <neosam@gmail.com>
2012-02-24 19:02:33 +0100

    Added Dimension classes

---
+++ src/com/gollersoft/jultragame/core/UGDimension.java
@@ -0,0 +1,20 @@
+package com.gollersoft.jultragame.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neosam
+ * Date: 2/24/12
+ * Time: 6:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UGDimension {
+    public int width, height;
+
+    public UGDimension(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public UGDimension() {
+    }
+}
---
+++ src/com/gollersoft/jultragame/core/UGFinalDimension.java
@@ -0,0 +1,17 @@
+package com.gollersoft.jultragame.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neosam
+ * Date: 2/24/12
+ * Time: 7:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UGFinalDimension {
+    public final int width, height;
+
+    public UGFinalDimension(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
