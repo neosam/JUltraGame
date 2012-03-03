7b3b3403e0d9228d8be51ca17f23cf60cff36d68
Simon Goller <neosam@gmail.com>
2012-03-03 15:32:12 +0100

    Added ActionMap.
    To connect action name strings with a runnable.

---
+++ src/com/gollersoft/jultragame/scene/ActionMap.java
@@ -0,0 +1,33 @@
+package com.gollersoft.jultragame.scene;
+
+import com.gollersoft.jultragame.core.UG;
+import com.gollersoft.jultragame.core.UGMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neosam-code
+ * Date: 3/3/12
+ * Time: 3:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ActionMap implements UGMap<String, Runnable> {
+    private final UGMap<String, Runnable> actionMap;
+
+    public ActionMap(UG ug) {
+        this.actionMap = ug.createMap();
+    }
+
+    @Override
+    public void put(String s, Runnable runnable) {
+        actionMap.put(s, runnable);
+    }
+
+    @Override
+    public Runnable get(String s) {
+        return actionMap.get(s);
+    }
+
+    public void executeAction(String action) {
+        actionMap.get(action).run();
+    }
+}
