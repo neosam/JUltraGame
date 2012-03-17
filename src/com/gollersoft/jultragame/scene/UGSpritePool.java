a8e4447439b6dfa0fbdea99423cb779068d29591
Simon Goller <neosam@gmail.com>
2012-03-17 12:25:14 +0100

    More functions for UGSpritePool
     * getSpritePoolItemAt:  return whole spritePoolItem at a index
     * getSpritePoolItem: return whole spritePoolItem which contains the given sprite
      * getSpritePoolItemsWithLabel:  Get the list of all SpritePoolItems filtered by the give label.

--- src/com/gollersoft/jultragame/scene/UGSpritePool.java
+++ src/com/gollersoft/jultragame/scene/UGSpritePool.java
@@ -35,6 +35,32 @@ public class UGSpritePool implements UGList<UGSprite> {
         return sprites.at(i).getSprite();
     }
 
+    public UGSpritePoolItem getSpritePoolItemAt(int i) {
+        return sprites.at(i);
+    }
+
+    public UGSpritePoolItem getSpritePoolItem(UGSprite sprite) {
+        for (int i = 0; i < sprites.size(); i++) {
+            if (sprites.at(i).getSprite() == sprite)
+                return sprites.at(i);
+        }
+        return null;
+    }
+
+    public UGList<UGSpritePoolItem> getSpritePoolItemsWithLabel(String label) {
+        final UGList<UGSpritePoolItem> spriteList = ug.createList();
+        for (int i = 0; i < sprites.size(); i++) {
+            final UGSpritePoolItem item = sprites.at(i);
+            for (int j = 0; j < item.getLabels().size(); j++) {
+                if (item.getLabels().at(i).equals(label)) {
+                    spriteList.add(item);
+                    break;
+                }
+            }
+        }
+        return spriteList;
+    }
+
     public void frame() {
 
     }
